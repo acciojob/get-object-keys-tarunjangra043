a6a951bdff39178363b98f7fd2ed06f96dcd3866
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-08-16 10:49:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const student = {
+    name: "Tarun"
+};
+
+Object.prototype.getKeys = function() {
+    return Object.keys(this);
+};
+
+console.log(student.getKeys()); 
